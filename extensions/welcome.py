ae1a2c2797f44795915f031953937ba9a74fba93
Norman FELTZ <norman.feltz@hotmail.fr>
2021-02-04 23:52:58 +0100

    Welcome: Optimise and add !welcome_unknow

--- extensions/welcome.py
+++ extensions/welcome.py
@@ -1,8 +1,11 @@
 import re
 import discord
 from discord.ext import commands
+from . import perms
 
 class WelcomeCog(commands.Cog):
+    utils_cog = None
+
     guild = None
     users = []
 
@@ -12,6 +15,7 @@ class WelcomeCog(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.utils_cog = self.bot.get_cog('UtilsCog')
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -44,31 +48,29 @@ class WelcomeCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        utils_cog = self.bot.get_cog('UtilsCog')
-
         dm_channel = member.dm_channel
 
         if dm_channel is None:
             dm_channel = await member.create_dm()
 
-        if utils_cog.settings.WELCOME_MODE == 'open':
+        if self.utils_cog.settings.WELCOME_MODE == 'open':
             msg = (
                 f"Bonjour {member.mention} vous débarquez ici, on dirait !\n"
                 f"Je suis {self.bot.user.mention}, je suis un gentil robot et je vais vous accompagner\n"
                 f"Tout d’abord pouvez-vous me donner l’adresse mail avec laquelle vous vous êtes inscrit(e) à l’évènement"
             )
-        elif utils_cog.settings.WELCOME_MODE == 'close':
+        elif self.utils_cog.settings.WELCOME_MODE == 'close':
             msg = (
                 f"Bonjour {member.mention} vous débarquez ici, on dirait !\n"
                 f"Je suis {self.bot.user.mention}, je suis le gentil robot du Hacking Industry Camp\n\n"
                 f"L'évenement n'a pas encore débuté ! Je reviendrai vers vous lors du lancement du HIC."
             )
+        
         await dm_channel.send(msg)
 
     @commands.Cog.listener()
     async def on_message(self, message):
         channel = message.channel
-        utils_cog = self.bot.get_cog('UtilsCog')
 
         if channel == self.channel_bdd_users:
             await self.loadUsers()
@@ -84,8 +86,7 @@ class WelcomeCog(commands.Cog):
             if len(member.roles) <= 1:
                 content = message.content.strip()
 
-                if utils_cog.settings.WELCOME_MODE == 'open':
-
+                if self.utils_cog.settings.WELCOME_MODE == 'open':
                     if not re.search('^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+$', content):
                         await channel.send("Je n'ai pas compris votre message :'(\nMerci de m'envoyer l'adresse email saisie lors de votre inscription au HIC.")
                         return
@@ -113,22 +114,46 @@ class WelcomeCog(commands.Cog):
                     ))
 
                     await self.channel_welcome.send(f"Bienvenue à {member.mention} sur le Discord du Hacking Industry Camp !")
-  
-
-                elif utils_cog.settings.WELCOME_MODE == 'close':
+                elif self.utils_cog.settings.WELCOME_MODE == 'close':
                     await channel.send((
                         f"Bonjour,\n"
                         f"Je ne suis pas en mesure de vous répondre avant le lancement du Hacking Industry Camp :'(\n"
                         f"Je reviendrai vers vous lors du lancement !"
                     ))
-
-
+    
     @commands.Cog.listener()
     async def on_raw_message_edit(self, payload):
         channel_id = payload.channel_id
 
         if channel_id == self.channel_bdd_users.id:
             await self.loadUsers()
+
+    @commands.command(name='welcome_unknown')
+    @commands.check(perms.is_support_user)
+    async def welcome_unknow(self, ctx):
+        """
+        Envoie un message à toutes les personnes qui n'ont pas de grade
+        """
+
+        if self.utils_cog.settings.WELCOME_MODE == 'close':
+            await ctx.send(f"Le mode Welcome est fermé !")
+            return
+
+        for member in self.guild.members:
+            if len(member.roles) <= 1:
+                dm_channel = member.dm_channel
+
+                if dm_channel is None:
+                    dm_channel = await member.create_dm()
+     
+                msg = (
+                    f"Bonjour {member.mention} vous débarquez ici, on dirait !\n"
+                    f"Je suis {self.bot.user.mention}, je suis un gentil robot et je vais vous accompagner\n"
+                    f"Tout d’abord pouvez-vous me donner l’adresse mail avec laquelle vous vous êtes inscrit(e) à l’évènement"
+                )
+
+                await dm_channel.send(msg)
+
 
 def setup(bot):
     bot.add_cog(WelcomeCog(bot))
