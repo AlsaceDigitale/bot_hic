ca5417be3bd603cd4d50a6f898196a6faac1c4d8
Norman FELTZ <norman.feltz@hotmail.fr>
2021-11-03 22:58:32 +0100

    Welcome: Fix missing settings in var

--- extensions/welcome.py
+++ extensions/welcome.py
@@ -25,7 +25,7 @@ class WelcomeCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        attendees = requests.get(self.utils_cog.URL_API_ATTENDEES).json()
+        attendees = requests.get(self.utils_cog.settings.URL_API_ATTENDEES).json()
 
         found_attendee = next((attendee for attendee in attendees if attendee["discord_unique_id"] == member.id), None)
 
@@ -47,7 +47,7 @@ class WelcomeCog(commands.Cog):
         await self.checkAttendees()
 
     async def checkAttendees(self):
-        attendees = requests.get(self.utils_cog.URL_API_ATTENDEES).json()
+        attendees = requests.get(self.utils_cog.settings.URL_API_ATTENDEES).json()
 
         async for member in self.guild.fetch_members(limit=None):
             found_attendee = next((attendee for attendee in attendees if attendee["discord_unique_id"] == member.id), None)
