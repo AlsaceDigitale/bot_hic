dd78f5818f1fdf0f6c2f4585faaa2515cb0decfe
Yannick Jost <yannick@yjost.com>
2023-11-22 23:29:49 +0100

    Make logs less verbose

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from sys import exc_info
 from flask import Flask, g
 
-#import Flask-APScheduler
+# import Flask-APScheduler
 from flask_apscheduler import APScheduler
 import apscheduler.events
 import apscheduler
 import apscheduler.schedulers.asyncio
 import flask_apscheduler
-
 
 import logging
 import structlog
@@ -19,26 +18,26 @@ import datetime
 # structlog.configure(logger_factory=LoggerFactory())  
 
 log = structlog.get_logger()
-log.warning("it works!", difficulty="easy")  
+log.warning("it works!", difficulty="easy")
 
 
 # set configuration values
 class Config(object):
     SCHEDULER_API_ENABLED = False
 
+
 # create app
 app = Flask(__name__)
 app.config.from_object(Config())
-
-
 
 # initialize scheduler
 scheduler = APScheduler()
 # if you don't wanna use a config, you can set options here:
 # scheduler.api_enabled = True
-#scheduler.init_app(app)
+# scheduler.init_app(app)
 
 import os
+
 
 @scheduler.task(trigger='interval', seconds=30, max_instances=1)
 def run_bot():
@@ -48,48 +47,44 @@ def run_bot():
     except Exception as e:
         log.error('bot crash', exc_info=e)
 
+
 def scheduler_event_listener(event: apscheduler.events.JobExecutionEvent):
-    log.info('scheduler event', evt=event)
     if event.exception:
         log.error('The job crashed :(', exc_info=event.exception)
-        
-    else:
-        log.info('The job worked :)')
 
-scheduler.add_listener(scheduler_event_listener, apscheduler.events.EVENT_JOB_EXECUTED | apscheduler.events.EVENT_JOB_ERROR)
 
-@scheduler.task(trigger='interval', seconds=60)
-def tick():
-    print('Tick! The time is: %s' % datetime.datetime.now())
-    
+scheduler.add_listener(scheduler_event_listener,
+                       apscheduler.events.EVENT_JOB_EXECUTED | apscheduler.events.EVENT_JOB_ERROR)
+
 log.info('jobs added', count=len(scheduler.get_jobs()))
+
 
 @app.route('/')
 def root():
     return "OK"
 
+
 @app.route('/jobs')
 def get_jobs():
-    job_info={
+    job_info = {
         "count": len(scheduler.get_jobs())
     }
     for job in scheduler.get_jobs():
-        d={
+        d = {
             "pending": job.pending,
             "id": job.id,
-            "next_run_time":str(job.next_run_time)
+            "next_run_time": str(job.next_run_time)
         }
-        job_info[job.id]=d
+        job_info[job.id] = d
     return job_info
+
 
 log.info('app configured')
 
 log.info('starting scheduler')
 scheduler.start()
 
-
 if __name__ == '__main__':
-    
     logging.basicConfig(
         format="%(message)s", stream=sys.stdout, level=logging.INFO
     )
@@ -103,5 +98,5 @@ if __name__ == '__main__':
         context_class=structlog.threadlocal.wrap_dict(dict),
         logger_factory=structlog.stdlib.LoggerFactory(),
     )
-    
+
     app.run()
